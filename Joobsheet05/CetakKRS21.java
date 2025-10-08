5048a9dc85a137fba3eaf435dccfdbafd5f5137b
pinkannanda <pinkannanda21@gmail.com>
2025-10-08 10:48:58 +0700

    Modifikasi Percobaan 1

--- Joobsheet05/CetakKRS21.java
+++ Joobsheet05/CetakKRS21.java
@@ -8,11 +8,17 @@ public class CetakKRS21 {
         Scanner sc = new Scanner(System.in);
       System.out.println("--- Cetak KRS SIAKAD ---");
       System.out.println("Apakah UKT sudah lunas? (true/false): ");
-      boolean uktLunas = sc.nextBoolean();
+      Boolean uktLunas = sc.nextBoolean();
 
-      if(uktLunas) {
+      if (uktLunas) {
         System.out.println("Pembayaran UKT terverifikasi");
         System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
+
+      }else {
+        System.out.println("Registrasi ditolak silakan, lunasi UKT terlebih dahuu");
+      }
       
          }   }
-    }
+        
+    
+      
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Hello world, ini file random saja
 
 Ini baris baru ditambahkan di GitHub 
-Hello GitHub, ini adalah repository pertama saya   
+Hello GitHub, ini adalah repository pertama saya  
