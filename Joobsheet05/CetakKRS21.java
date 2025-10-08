b5b18cbc06738b9e36522a7700efef996ea6b950
pinkannanda <pinkannanda21@gmail.com>
2025-10-08 10:23:39 +0700

    Percobaan 1

---
+++ Joobsheet05/CetakKRS21.java
@@ -0,0 +1,18 @@
+package Joobsheet05;
+
+import java.util.Scanner;
+
+public class CetakKRS21 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+      System.out.println("--- Cetak KRS SIAKAD ---");
+      System.out.println("Apakah UKT sudah lunas? (true/false): ");
+      boolean uktLunas = sc.nextBoolean();
+
+      if(uktLunas) {
+        System.out.println("Pembayaran UKT terverifikasi");
+        System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
+      
+         }   }
+    }
