926405414dc2241e18ecf5b887bd228a2e057253
pinkannanda <pinkannanda21@gmail.com>
2025-10-08 11:13:19 +0700

    Percobaan 1 Ternary

--- Joobsheet05/CetakKRS21.java
+++ Joobsheet05/CetakKRS21.java
@@ -6,19 +6,22 @@ public class CetakKRS21 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-      System.out.println("--- Cetak KRS SIAKAD ---");
-      System.out.println("Apakah UKT sudah lunas? (true/false): ");
-      Boolean uktLunas = sc.nextBoolean();
+        System.out.println("--- Cetak KRS SIAKAD ---");
+        System.out.println("Apakah UKT sudah lunas? (true/false): ");
+        Boolean uktLunas = sc.nextBoolean();
 
-      if (uktLunas) {
-        System.out.println("Pembayaran UKT terverifikasi");
-        System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
+        // if (uktLunas) {
+        // System.out.println("Pembayaran UKT terverifikasi");
+        // System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
 
-      }else {
-        System.out.println("Registrasi ditolak silakan, lunasi UKT terlebih dahuu");
-      }
-      
-         }   }
-        
-    
-      
+        // } else {
+        // System.out.println("Registrasi ditolak silakan, lunasi UKT terlebih dahuu");
+        // }
+
+        String hasil = uktLunas
+                ? "Pembayaran UKT Terverifikasi\nSilahkan cetak KRS dan minta tanda tangan DPA"
+                : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu";
+        System.out.println(hasil);
+
+    }
+}
