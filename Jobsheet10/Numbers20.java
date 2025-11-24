c21168f0b2634201aadbb5ef860e8c735d859394
pinkannanda <pinkannanda21@gmail.com>
2025-11-24 18:17:32 +0700

    SIAKAD

---
+++ Jobsheet10/Numbers20.java
@@ -0,0 +1,17 @@
+package Jobsheet10;
+import java.util.Arrays;
+import java.util.Scanner;
+public class Numbers20 {
+    public static void main(String[] args) {
+        int[][] myNumbers = new int[3][];
+        myNumbers[0] = new int[5];
+        myNumbers[1] = new int[3];
+        myNumbers[2] = new int[1];
+
+        for (int i = 0; i < myNumbers.length; i++){
+            System.out.println("Panjang beris ke-" + ": " + myNumbers[i].length);
+        }
+
+        
+    }
+}
